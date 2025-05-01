13a135e305d1836d3ce4f633a56c1314d8931276
grbrielcosta <gvbrielcosta@gmail.com>
2025-05-01 10:21:24 -0300

    feat: created user entity for security configuration

---
+++ src/main/java/br/com/movieflix/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package br.com.movieflix.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
---
+++ src/main/java/br/com/movieflix/entity/User.java
@@ -0,0 +1,27 @@
+package br.com.movieflix.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
