5abaf389cc4960647fcf3c8edc474a5c92072f8a
grbrielcosta <gvbrielcosta@gmail.com>
2025-06-02 17:48:20 -0300

    feat: all of security configs implemented. jwt is now required for requests

--- src/main/java/br/com/movieflix/config/TokenService.java
+++ src/main/java/br/com/movieflix/config/TokenService.java
@@ -3,10 +3,13 @@ package br.com.movieflix.config;
 import br.com.movieflix.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -26,5 +29,23 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("API MOVIEFLIX")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken(String token) {
+        try {
+            DecodedJWT jwt = JWT.require(Algorithm.HMAC256(secret))
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("userid").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .build());
+
+        } catch (JWTVerificationException exception) {
+            return Optional.empty();
+        }
     }
 }
